1c840dbccbda8e58804e17e9a8ec6a5be3faafbb
mkarijanna <78878193+mkarijanna@users.noreply.github.com>
2021-03-24 15:06:02 -0500

    Updated syscall.h
    Added methods from void halt() to bool remove() in order of the Project 2 description

--- pintos/src/userprog/syscall.h
+++ pintos/src/userprog/syscall.h
@@ -3,4 +3,13 @@
 
 void syscall_init (void);
 
+/* --------- Karijanna's code starts here ---------- */
+typedef int pid_t;
+void halt(void);
+void exit (int status);
+pid_t exec (const char *cmd_line);
+int wait (pid_t pid);
+bool create (const char *file, unsigned initial_size);
+bool remove (const char *file);
+
 #endif /* userprog/syscall.h */
