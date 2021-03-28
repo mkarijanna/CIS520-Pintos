95396f28863878e4a449592957d79201d3eaf653
Kelcie Dinkel <kdinkel08@outlook.com>
2021-03-28 15:58:42 -0500

    test_3

--- pintos/src/threads/thread.c
+++ pintos/src/threads/thread.c
@@ -482,7 +482,7 @@ init_thread (struct thread *t, const char *name, int priority)
   t->priority = priority;
   t->magic = THREAD_MAGIC;
   #ifdef USERPROG
-  list_init( &file_descriptors );
+  list_init( &( t->file_descriptors ) );
   #endif
   old_level = intr_disable ();
   list_push_back (&all_list, &t->allelem);
